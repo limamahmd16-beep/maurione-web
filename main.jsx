ec56c07598eb6f1438e3b21116a33a98e53fb35c
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-01 17:16:56 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MauriOneApp from "./MauriOne.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MauriOneApp />
+  </React.StrictMode>
+);
