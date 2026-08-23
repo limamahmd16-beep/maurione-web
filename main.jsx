9df2fc750877eeb023dd3b9d7d1564ebc289515e
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-23 21:27:35 +0100

    Load MauriOne premium animation layer

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import MauriOneApp from "./MauriOne.jsx";
+import "./polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
