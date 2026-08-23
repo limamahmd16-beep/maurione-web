316c5c3fc9392dcc1a829845d567c66c744fd9d3
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-23 23:14:10 +0100

    Load MauriOne official brand identity styles

--- main.jsx
+++ main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import MauriOneApp from "./MauriOne.jsx";
 import "./polish.css";
+import "./brand.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
