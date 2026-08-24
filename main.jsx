d25496b4f0369e72e17e9b8409ec531d72cdf710
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-24 21:07:45 +0100

    Enable MauriOne premium motion preview

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MauriOneApp from "./MauriOneLight.jsx";
+import MauriOneApp from "./MauriOneMotion.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
