f733197465de11a304b9eec422e1428cab0b7ba8
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-24 19:58:21 +0100

    Use light-first MauriOne app wrapper

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MauriOneApp from "./MauriOne.jsx";
+import MauriOneApp from "./MauriOneLight.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
