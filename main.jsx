e09046a684c02fdd14a604c89db712cb2f424205
limamahmd16-beep <limamahmd16@gmail.com>
2026-08-24 21:52:14 +0100

    Route admin to linked Firebase showroom panel

--- main.jsx
+++ main.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import MauriOneApp from "./MauriOneMotion.jsx";
+import ShowroomAdmin from "./ShowroomAdmin.jsx";
+
+function RootRouter() {
+  const [path, setPath] = useState(() => window.location.pathname);
+
+  useEffect(() => {
+    const onRoute = () => setPath(window.location.pathname);
+    window.addEventListener("popstate", onRoute);
+    return () => window.removeEventListener("popstate", onRoute);
+  }, []);
+
+  const isAdminRoute = path === "/admin" || path.startsWith("/admin/");
+  return isAdminRoute ? <ShowroomAdmin /> : <MauriOneApp />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MauriOneApp />
+    <RootRouter />
   </React.StrictMode>
 );
